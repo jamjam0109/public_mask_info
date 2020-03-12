d4ef3f150fac8696b6392ed2d5fdc1b0d29980b3
Jaemin <zxc16199@gmail.com>
2020-03-12 17:31:00 +0900

    sample 이미지 추가

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from api import storesByAddr, storesByGeo
 
-address = '경기도 성남시 분당구 수내동'
-save_path = '../storesByAddr_output.csv'
+# 주소 기반
+address = '경기도 성남시 분당구 야탑동'
+save_path = '../storesByAddr_output/storesByAddr_output.csv'
+
 storesByAddr(address, save_path)
 
 
+# 위도/경도 기반
 lat = 37.411975
 lng = 127.1465933
 m = 1000
 save_path = '../storesByGeo_output.csv'
+
 storesByGeo(lat, lng, m, save_path)
